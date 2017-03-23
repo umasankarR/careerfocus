63f6ca1dd94d0072eda288da4035f63fe69fff6b
umasankar <uma.sankar430@gmail.com>
2017-03-23 22:35:02 +0530

    initial

---
+++ CF-RESTFUL/src/main/java/com/careerfocus/CfRestfulApplication.java
@@ -0,0 +1,12 @@
+package com.careerfocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfRestfulApplication.class, args);
+	}
+}
